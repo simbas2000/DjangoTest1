c71c9224729c9f48a641f5b3de7748de84572a48
Simone Baselice Mac <s.baselice@simonebaselice.it>
2019-07-12 19:14:14 +0200

    helper function 'check_for_row_in_list_table' added in functional_test.py

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,12 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		print(rows)
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrive_it_later(self):
 		#User visits homepage
 		self.browser.get('http://localhost:8000')
@@ -28,15 +34,12 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys('Item 1')
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(0.02)
+		self.check_for_row_in_list_table('1: Item 1')
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Item 2')
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(0.02)
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue( any(row.text == '1: Item 1' for row in rows), "New to-do item(1) did not appear in table!" )
-		self.assertTrue( any(row.text == '2: Item 2' for row in rows), "New to-do item(2) did not appear in table!" )
-
+		self.check_for_row_in_list_table('2: Item 2')
 
 		self.fail('Finish the test!')
 
