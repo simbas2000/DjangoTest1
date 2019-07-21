173e4893773752992f5c9634c57af046f78a017b
Simone Baselice Mac <s.baselice@simonebaselice.it>
2019-07-21 20:46:54 +0200

    refactoring.. new template for visualizing lists

--- lists/tests.py
+++ lists/tests.py
@@ -7,77 +7,84 @@ from lists.models import Item
 import re
 
 def remove_csrf_tag(text):
-	return re.sub(r'<[^>]*csrfmiddlewaretoken[^>]*>', '', text)
+    return re.sub(r'<[^>]*csrfmiddlewaretoken[^>]*>', '', text)
+
+class ListViewTest(TestCase):
+    
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'item 1')
+        self.assertContains(response, 'item 2')
 
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		expected_html = render(request, 'home.html').content.decode()
-		self.assertEqual(remove_csrf_tag(response.content.decode()), remove_csrf_tag(expected_html))
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render(request, 'home.html').content.decode()
+        self.assertEqual(remove_csrf_tag(response.content.decode()), remove_csrf_tag(expected_html))
 
-	def test_home_page_can_save_a_POST_request(self):
-		#Setup
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
+    def test_home_page_can_save_a_POST_request(self):
+        #Setup
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
 
-		#Exercise
-		response = home_page(request)
+        #Exercise
+        response = home_page(request)
 
-		#Assert
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new list item')
+        #Assert
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
-	def test_home_page_redirects_after_POST(self):
-		#Setup
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
+    def test_home_page_redirects_after_POST(self):
+        #Setup
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
 
-		#Exercise
-		response = home_page(request)
+        #Exercise
+        response = home_page(request)
 
-		#Assert
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/')
+        #Assert
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
-	def test_home_page_displays_all_list_items(self):
-		Item.objects.create(text='item 1')
-		Item.objects.create(text='item 2')
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
-		request = HttpRequest()
-		response = home_page(request)
 
-		self.assertIn('item 1', response.content.decode())
-		self.assertIn('item 2', response.content.decode())
-
-	def test_home_page_only_saves_items_when_necessary(self):
-		request = HttpRequest()
-		home_page(request)
-		self.assertEqual(Item.objects.count(), 0)
 class ItemModelTest(TestCase):
 
-	def test_saving_and_retriving_items(self):
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.save()
+    def test_saving_and_retriving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
 
-		second_item = Item()
-		second_item.text = 'The second item'
-		second_item.save()
+        second_item = Item()
+        second_item.text = 'The second item'
+        second_item.save()
 
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
 
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-		self.assertEqual(second_saved_item.text, 'The second item')
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'The second item')
--- superlists/urls.py
+++ superlists/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from lists import views
 
 urlpatterns = [
-   #path('admin/', admin.site.urls),
-	path('', views.home_page, name='home'),
+    #path('admin/', admin.site.urls),
+    path('', views.home_page, name='home'),
+    re_path(r'^lists/the-only-list-in-the-world/$', views.view_list,
+            name='view_list'),
 ]
