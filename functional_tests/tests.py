fffd17119c6e921688394b2f9e3d8673277aa20f
Simone Baselice Mac <s.baselice@simonebaselice.it>
2019-07-28 01:07:55 +0200

    FT can now also test against a staging server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,24 @@
+import sys
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+            super().setUpClass()
+            cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +34,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         
     def test_can_start_a_list_and_retrive_it_later(self):
         #User visits homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         
         #User notices title and header
         self.assertIn('To-Do', self.browser.title)
@@ -45,7 +60,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('1: Item 1', page_text)
         self.assertNotIn('2: Item 2', page_text)
@@ -64,7 +79,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('l2_i1', page_text)
 
     def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
