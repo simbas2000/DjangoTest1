89f32144e6bda290e0688541fa13d0602a33927a
Simone Baselice Mac <s.baselice@simonebaselice.it>
2019-08-17 23:19:45 +0200

    Fabfile.py now seems to be working

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -9,7 +9,7 @@ REPO_URL = 'https://github.com/simbas2000/DjangoTest1.git'
 
 
 def _exists(c: Connection, path):
-    result = c.run('test -a {}/.git && echo 1 || echo 0'.format(path))
+    result = c.run('test -a {} && echo 1 || echo 0'.format(path))
     return result.stdout.strip() == "1"
 
 
@@ -24,7 +24,7 @@ def _get_latest_source(c: Connection, source_folder):
     else:
         c.run('git clone {} {}'.format(REPO_URL, source_folder))
     current_commit = local_run('git log -n 1 --format=%H')  # LAUNCH FAB FROM LOCAL REPO DIR AFTER PUSH
-    c.run('cd {} && git reset --hard {}'.format((source_folder, current_commit.stdout.strip())))
+    c.run('cd {} && git reset --hard {}'.format(source_folder, current_commit.stdout.strip()))
 
 
 def _update_settings(c: Connection, source_folder, sitename):
@@ -36,7 +36,7 @@ def _update_settings(c: Connection, source_folder, sitename):
     loc_secret_key_path = loc_tmp_dir + '/secret_key.py'
     os.mkdir(loc_tmp_dir)
     try:
-        c.get(settings_path, local=os.getcwd()+'/'+loc_tmp_dir)
+        c.get(settings_path, local=os.getcwd()+'/'+loc_tmp_dir+'/settings.py')
         os.rename(loc_tmp_dir+'/settings.py', loc_old_settings_path)
         with open(loc_old_settings_path, 'r') as f:
             content = f.read()
@@ -49,7 +49,7 @@ def _update_settings(c: Connection, source_folder, sitename):
                 chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
                 key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
                 with open(loc_secret_key_path, 'w') as nkey:
-                    nkey.write(key)
+                    nkey.write('SECRET_KEY = "{}"'.format(key))
                 c.put(os.getcwd()+'/'+loc_secret_key_path, remote=source_folder+'/superlists/')
             c.put(os.getcwd()+'/'+loc_new_settings_path, remote=source_folder+'/superlists/')
     finally:
@@ -75,9 +75,9 @@ def _update_database(c: Connection, source_folder):
 def deploy(c):
     site_folder = '/home/%s/sites/%s' % (c.user, c.host)
     source_folder = site_folder + '/source'
-    _create_directory_structure_if_necessary(site_folder)
-    _get_latest_source(source_folder)
-    _update_settings(source_folder, c.host)
-    _update_virtualenv(source_folder)
-    _update_static_files(source_folder)
-    _update_database(source_folder)
+    _create_directory_structure_if_necessary(c, site_folder)
+    _get_latest_source(c, source_folder)
+    _update_settings(c, source_folder, c.host)
+    _update_virtualenv(c, source_folder)
+    _update_static_files(c, source_folder)
+    _update_database(c, source_folder)
