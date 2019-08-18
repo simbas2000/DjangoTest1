c52f701a5ca5074e711e45085aad7a0bfd970d72
Simone Baselice <simbas2000@gmail.com>
2019-08-18 21:17:15 +0200

    Moved FTs in individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,48 +1,22 @@
-import sys
+from .base import FunctionalTest
 import time
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(StaticLiveServerTestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
-                return
-            super().setUpClass()
-            cls.server_url = cls.live_server_url
+class NewVisitorTest(FunctionalTest):
 
-    @classmethod
-    def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
-            super().tearDownClass()
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-        
     def test_can_start_a_list_and_retrive_it_later(self):
-        #User visits homepage
+        # User visits homepage
         self.browser.get(self.server_url)
-        
-        #User notices title and header
+
+        # User notices title and header
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        time.sleep(0.02) #seems needed to avoid bug with selenium (?)
-        
-        #User enter items in list using inputbox
+        time.sleep(0.02)  # seems needed to avoid bug with selenium (?)
+
+        # User enter items in list using inputbox
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         inputbox.send_keys('Item 1')
@@ -78,17 +52,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('1: Item 1', page_text)
         self.assertIn('l2_i1', page_text)
 
-    def test_layout_and_styling(self):
-        self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768)
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5
-        )
-
-        inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5
-        )
