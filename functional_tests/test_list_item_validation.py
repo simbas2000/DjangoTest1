03377e20073e083645b1fcbffc29e1114f50a582
Simone Baselice Mac <s.baselice@simonebaselice.it>
2019-08-29 12:00:24 +0200

    model: unique items in list

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -13,6 +13,7 @@ class ItemValidationTest(FunctionalTest):
         time.sleep(.2)
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
+        self.assertIn('is-invalid', self.browser.page_source)
         error = self.browser.find_element_by_class_name("invalid-feedback")
         self.assertEqual(error.text, "You can't have an empty list item")
 
@@ -28,5 +29,20 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         self.get_item_input_box().send_keys('Item 2' + Keys.ENTER)
+        time.sleep(.2)
         self.check_for_row_in_list_table('1: Item 1')
         self.check_for_row_in_list_table('2: Item 2')
+        
+    def test_cannot_add_duplicate_items(self):
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('itm1'+Keys.ENTER)
+        time.sleep(.2)
+        self.check_for_row_in_list_table('1: itm1')
+        # User accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys('itm1'+Keys.ENTER)
+        time.sleep(.2)
+        self.check_for_row_in_list_table('1: itm1')
+        self.assertIn('is-invalid', self.browser.page_source)
+        error = self.browser.find_element_by_class_name("invalid-feedback")
+        self.assertEqual(error.text, "You've already got this in your list")
+
--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -38,6 +38,22 @@ class ListAndItemModelTest(TestCase):
         with self.assertRaises(ValidationError):
             item.save()
             item.full_clean()
+        
+
+    def test_duplicate_items_are_invalid(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='bla')
+        with self.assertRaises(ValidationError):
+            item = Item(list=list_, text='bla')
+            item.full_clean()
+
+    
+    def test_CAN_save_same_item_to_different_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1, text='bla')
+        item = Item(list=list2, text='bla')
+        item.full_clean() # should not raise
 
 
     def test_get_absolute_url(self):
