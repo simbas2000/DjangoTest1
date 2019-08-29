60842237dcdb65fd1c0e12ff21f02810163cc3f1
Simone Baselice <simbas2000@gmail.com>
2019-08-29 15:05:05 +0200

    Stetting up js test system

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -2,8 +2,15 @@ from .base import FunctionalTest
 import time
 from selenium.webdriver.common.keys import Keys
 
+
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        if 'is-invalid' in self.browser.page_source:
+            error = self.browser.find_element_by_class_name("invalid-feedback")
+            return error.text
+        else:
+            return None
 
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.server_url)
@@ -13,9 +20,7 @@ class ItemValidationTest(FunctionalTest):
         time.sleep(.2)
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
-        self.assertIn('is-invalid', self.browser.page_source)
-        error = self.browser.find_element_by_class_name("invalid-feedback")
-        self.assertEqual(error.text, "You can't have an empty list item")
+        self.assertEqual(self.get_error_element(), "You can't have an empty list item")
 
         self.get_item_input_box().send_keys('Item 1' + Keys.ENTER)
         self.check_for_row_in_list_table('1: Item 1')
@@ -25,24 +30,31 @@ class ItemValidationTest(FunctionalTest):
 
         time.sleep(.2)
         self.check_for_row_in_list_table('1: Item 1')
-        error = self.browser.find_element_by_class_name("invalid-feedback")
-        self.assertEqual(error.text, "You can't have an empty list item")
+        self.assertEqual(self.get_error_element(), "You can't have an empty list item")
 
         self.get_item_input_box().send_keys('Item 2' + Keys.ENTER)
         time.sleep(.2)
         self.check_for_row_in_list_table('1: Item 1')
         self.check_for_row_in_list_table('2: Item 2')
-        
+
     def test_cannot_add_duplicate_items(self):
         self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('itm1'+Keys.ENTER)
+        self.get_item_input_box().send_keys('itm1' + Keys.ENTER)
         time.sleep(.2)
         self.check_for_row_in_list_table('1: itm1')
         # User accidentally tries to enter a duplicate item
-        self.get_item_input_box().send_keys('itm1'+Keys.ENTER)
+        self.get_item_input_box().send_keys('itm1' + Keys.ENTER)
         time.sleep(.2)
         self.check_for_row_in_list_table('1: itm1')
-        self.assertIn('is-invalid', self.browser.page_source)
-        error = self.browser.find_element_by_class_name("invalid-feedback")
-        self.assertEqual(error.text, "You've already got this in your list")
+        self.assertEqual(self.get_error_element(), "You've already got this in your list")
 
+    def test_error_messages_are_cleared_on_input(self):
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        time.sleep(.2)
+        self.assertIsNotNone(self.get_error_element())
+
+        self.get_item_input_box().send_keys('a')
+
+        time.sleep(.2)
+        self.assertIsNone(self.get_error_element())
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,7 +5,7 @@ from lists.views import home_page
 from django.shortcuts import render
 from django.utils.html import escape
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_LIST_ERROR
+from lists.forms import ItemForm, EMPTY_LIST_ERROR, ExistingListItemForm, DUPLICATE_ITEM_ERROR
 import re
 
 
@@ -129,17 +129,26 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/{}/'.format(list_.id))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_LIST_ERROR))
+
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='txt')
+        response = self.client.post('/lists/{}/'.format(list1.id), data={'text': 'txt'})
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
 class HomePageTest(TestCase):
