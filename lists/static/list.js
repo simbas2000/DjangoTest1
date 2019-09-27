b63ad1c2f8978d80bfae3d2d12ad7e3ca5c05a62
Simone Baselice Mac <s.baselice@simonebaselice.it>
2019-09-27 20:29:03 +0200

    Bugfix- validation error not displayed

--- lists/static/list.js
+++ lists/static/list.js
@@ -7,11 +7,22 @@ function lvalidator_init() {
     var c_name = '.user_input_form'
     $(c_name).find("input").on("keypress", function () {
         $(c_name).find("input").removeClass('is-invalid').removeClass('is-valid')
+        $(c_name).find(".invalid-feedback").remove();
     })
     $(c_name).find("input").on("blur", function () {
         $(c_name).find("input").each(function () {
-            if (!isInputValid($(this).val()))
+            if (!isInputValid($(this).val())) {
                 $(this).addClass("is-invalid");
+                if(!$(c_name).find(".invalid-feedback").text().includes("empty")) {
+                    $(c_name).append(`
+                        <div class="invalid-feedback">
+                            <div class="float-left">
+                               You can't have an empty list item
+                            </div>
+                         </div>
+                    `)
+                }
+            }
         })
     })
 }
